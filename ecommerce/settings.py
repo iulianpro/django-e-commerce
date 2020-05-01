04844bb1d4c061592f16438c775929b127ba8a66
iulianpro <iulian.prodea@gmail.com>
2020-05-02 00:27:06 +0100

    Added Products app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
+    'products',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
